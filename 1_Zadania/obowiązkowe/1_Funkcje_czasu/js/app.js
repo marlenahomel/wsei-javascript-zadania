062be0bf43b4ed2909f331dac56f2e63c7af969a
marlena.homel <marlena.homel@gmail.com>
2019-10-06 11:53:36 +0200

    final version

--- 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -1,16 +1,8 @@
-fuction countHello(number){
-
+function countHello(number){
     var number2 = 1
-    if (number > number2) {
-        setInterval(function () {
-            console.log("Hello" + number);
-            number--;
-        }, 1000)
-    }
-    else
-        clearInterval;
-
-
-
+       const interval = setInterval(function () {
+            console.log("Hello" + number2++);
+            if(number2===number+1) clearInterval(interval);
+       }, 1000)
 }
 countHello(10);
